fa6cf7036984fbe6d2ea64625acd9c00872ecc91
Sergiu Tigan <sergiutigan@gmail.com>
2020-06-11 15:03:08 +0300

    Finished Page

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-const banner = document.querySelector('.banner');
+//navbar animation smooth
+const content = document.querySelector('.content');
 const nav = document.querySelector('header');
 const options = {
     root: null,
     threshold: 0,
-    rootMargin: "-150px"
+    rootMargin: "-300px"
 };
 
 const obs = new IntersectionObserver((entries, obs) => {
     entries.forEach(entry => {
+        nav.style.transition = '0.3s';
         nav.classList.toggle("bla");
     });
 }, options);
 
-obs.observe(banner);
+obs.observe(content);
