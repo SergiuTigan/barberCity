d09bfd2b2b6a18ab1f6f5f4a467d1a4e8ba5d23e
Sergiu Tigan <sergiutigan@gmail.com>
2020-06-26 11:00:24 +0300

    banner

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 //navbar animation smooth
 const content = document.querySelector('.content');
 const nav = document.querySelector('header');
+const menuItems = document.querySelectorAll('.hoverC');
+const toggler = document.querySelector('.toggler');
+const clocks = document.querySelectorAll('.fa-clock-o');
 const options = {
     root: null,
     threshold: 0,
@@ -10,8 +13,26 @@ const options = {
 const obs = new IntersectionObserver((entries, obs) => {
     entries.forEach(entry => {
         nav.style.transition = '0.3s';
-        nav.classList.toggle("bla");
+        nav.classList.toggle("fixed");
     });
 }, options);
 
 obs.observe(content);
+
+for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', () => {
+        toggler.checked = false;
+    })
+}
+
+function removeClocks(small) {
+    if (small.matches) {
+        for (let i = 0; i < clocks.length; i++) {
+            clocks[i].style.display = "none";
+        }
+    }
+}
+
+let small = window.matchMedia("(max-width:1199px)");
+removeClocks(small);
+small.addListener(removeClocks);
